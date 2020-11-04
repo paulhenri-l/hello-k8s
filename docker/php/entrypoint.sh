b539562b2ef13fa14ae4ddbe6ff2948722c68d06
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2020-11-04 23:00:59 +0100

    Remove pipe thing

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ ! -p /app/storage/logs/laravel.log ]; then
-    mkfifo /app/storage/logs/laravel.log
-    chmod 777 /app/storage/logs/laravel.log
-fi
-
 php artisan optimize
 php artisan view:cache
 
--- docker/tail/tail.sh
+++ docker/tail/tail.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Wait until the pipe - created by the PHP app container
-# and shared via a tmp volume - is created
 while [ ! -e /app/storage/logs/laravel.log ]; do
     sleep 0.2
 done
